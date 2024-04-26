43a9d5a9cdb64b07dfb6830f8a8cb35bcacd4686
ZahwaAnritia <anritia15@gmail.com>
2024-04-26 10:00:48 +0700

    menambahkan kondisi untuk penanganan jika NIM sudah ada dalam linked list

--- 116_singlelinkedlist/116_singlelinkedlist.cpp
+++ 116_singlelinkedlist/116_singlelinkedlist.cpp
@@ -37,7 +37,13 @@ void addNode() {
 
 	while ((current != NULL) && (nim >= current->noMhs)) 
 	{
-
+		if (nim == current->noMhs)
+		{
+			cout << "NIM sudah ada" << endl;
+			return;
+		}
+		previous = current;
+		current = current->next;
 	}
 }
 
